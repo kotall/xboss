1afa0108be1cc90fd4d3077e7a84e641afcc6ae4
aracwong <aracwong@163.com>
2018-08-26 18:27:43 +0800

    add readme.

--- auth-server/src/main/java/com/github/kotall/auth/Oauth2Application.java
+++ auth-server/src/main/java/com/github/kotall/auth/Oauth2Application.java
@@ -2,11 +2,7 @@ package com.github.kotall.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * @author zpwang
@@ -21,14 +17,4 @@ public class Oauth2Application {
     }
 
 
-    @Configuration
-    @EnableResourceServer
-    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            http.antMatcher("/user").authorizeRequests().anyRequest().authenticated();
-            // @formatter:on
-        }
-    }
 }
