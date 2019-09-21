8ec6b2579600864546b176025a707a8faa2f04cd
skyarac <haiyasheji@163.com>
2019-09-21 21:48:19 +0800

    项目结构重构

---
+++ xboss-shop/src/main/java/com/github/kotall/shop/ShopAppStarter.java
@@ -0,0 +1,17 @@
+package com.github.kotall.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author: zpwang
+ * @time: 2019/9/21 21:41
+ */
+@SpringBootApplication
+public class ShopAppStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAppStarter.class, args);
+    }
+}
